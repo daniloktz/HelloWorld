1f8d9efe8f9978a06de1ef48d02377f1f8b25cbd
Danilo Katzaroff <daniloktz@gmail.com>
2018-01-29 11:13:24 -0300

    Create intersecao de arquivos para auditoria.r
    Script rapido para fazer intercecao de itens para auditoria
    Por exemplo:
    
    base a = base fiscal
    base b = base auditada
    
    retornara:
    base correta
    base registrada e nao localizada
    base localizada e nao registrada (pode ser typo)

---
+++ intersecao de arquivos para auditoria.r
@@ -0,0 +1,15 @@
+baseA <- read.csv("C:\\Users\\Corporativo\\baseA.csv")
+baseB <- read.csv("C:\\Users\\Corporativo\\baseB.csv")
+#le os dois arquivos
+
+ReferenciasIguais <- intersect(baseA[1],baseB[1])
+ReferenciasIguais
+#faz a intersecao, de acordo com a primeira coluna, de ambas as bases. voce pode alterar a coluna alterando o numero de cada base
+
+SobaseA <- setdiff(baseA[1],baseB[1])
+SobaseA
+#remove de A o que tem em B
+
+SobaseB <- setdiff(baseB[1],baseA[1])
+SobaseB
+#remove de B o que tem em A 
